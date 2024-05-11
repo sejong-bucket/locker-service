70a3d407bf46d61c16908caf0d07fbd1c97e3a5a
jaepyo99 <jaepyo99@naver.com>
2024-05-12 01:39:52 +0900

    init

---
+++ src/main/java/com/sejongbucket/lockerservice/LockerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sejongbucket.lockerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockerServiceApplication.class, args);
+    }
+
+}
